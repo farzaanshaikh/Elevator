d6935dd1553ce1c96e951d6ade82cd159cd31143
Farzaan Shaikh <fvshaikh93@gmail.com>
2023-01-28 15:35:02 +0530

    Created core app and built models

---
+++ elevator/elevator_manager/constants.py
@@ -0,0 +1,15 @@
+##############################################################  DB Model Constants  #########################################################################
+
+GROUND_FLOOR = 0
+BUILDING_MIN_FLOORS = 1
+BUILDING_MAX_FLOORS = 60
+
+BUILDING_MIN_ELEVATORS = 1
+BUILDING_MAX_ELEVATORS = 10
+
+ELEVATOR_DOOR_OPEN = True
+ELEVATOR_DOOR_CLOSED = False
+
+ELEVATOR_STOPPED = 0
+ELEVATOR_MOVE_UP = 1
+ELEVATOR_MOVE_DOWN = 2
